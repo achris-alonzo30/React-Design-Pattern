c8d04ab6d2163b2044774c84f0d94a1773daed6a
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-01 12:14:44 -0300

    3: implement list and list items with different list types

---
+++ src/Components/NumberedList.jsx
@@ -0,0 +1,16 @@
+export const NumberList = ({
+  items,
+  resourceName,
+  itemComponent: ItemComponent,
+}) => {
+  return ( 
+    <>
+      {items.map((item, i) => (
+        <div className="flex flex-between items-center gap-2">
+          <h3 className="flex text-center items-center justify-center text-lg text-slate-500 font-bold border-2 border-slate-500 rounded-full w-10 h-10">{i + 1}</h3>
+          <ItemComponent key={i} {...{ [resourceName]: item }} />
+        </div>
+      ))}
+    </>
+  );
+};
---
+++ src/Components/person/LargePersonListItem.jsx
@@ -0,0 +1,24 @@
+export const LargePersonListItem = ({ person }) => {
+  const { name, age, hairColor, hobbies } = person;
+  return (
+    <aside className="flex flex-col items-center border border-slate-500 rounded-lg py-1 my-4 bg-white">
+      <hgroup className="flex flex-col items-center text-start gap-2">
+        <h4 className="text-lg text-slate-500">
+          <strong>Name</strong>: {name}
+        </h4>
+        <p className="text-lg text-slate-500">
+          <strong>Age</strong>: {age}
+        </p>
+        <p className="text-lg text-slate-500">
+          <strong>Hair Color</strong>: {hairColor}
+        </p>
+        <ul className="flex gap-2 text-lg text-slate-500">
+          <strong>Hobbies: </strong>
+          {hobbies.map((hobby, i) => (
+            <li key={i}>{hobby}</li>
+          ))}
+        </ul>
+      </hgroup>
+    </aside>
+  );
+};
