b5d2d6bd0d6fe31b56ad6bcda552664b5d177ad9
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-02 08:11:15 -0300

    1: reset back to old state

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import "./App.css";
-import { LeftComponent } from "./Components/LeftComponent";
-import { RightComponent } from "./Components/RightComponent";
-import { SplitScreen } from "./Components/SplitScreen";
-import { Modal } from './Components/modal/Modal';
-import { LargePersonListItem } from './Components/person/LargePersonListItem';
-import { people } from '../lib/data';
+import { CurrentUserLoader } from "./CurrentUserLoader";
+
 function App() {
   return (
     <main className="flex flex-col p-6 bg-background gap-4">
       <header className="text-center">
         <h1 className="font-bold text-4xl text-slate-500">
-          Layout Components
+          Container Components
         </h1>
-        <Modal>
-          <LargePersonListItem person={people[0]} />
-        </Modal>
       </header>
-      <SplitScreen>
-        <LeftComponent />
-        <RightComponent />
-      </SplitScreen>
+      <CurrentUserLoader />
     </main>
   );
 }
